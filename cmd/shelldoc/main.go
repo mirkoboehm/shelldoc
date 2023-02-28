5b74db09e8b5b8a64255ad202b84bb69c4fab07f
Mirko Boehm <mirko.boehm@mercedes-benz.com>
2023-02-28 21:16:49 +0100

    Fix path

--- cmd/shelldoc/main.go
+++ cmd/shelldoc/main.go
@@ -1,10 +1,10 @@
 // This file is part of shelldoc.
-// © 2019, Mirko Boehm <mirko@endocode.com> and the shelldoc contributors
+// © 2023, Mirko Boehm <mirko@kde.org> and the shelldoc contributors
 // SPDX-License-Identifier: GPL-3.0
 
 package main
 
-import "github.com/mirkoboehm/shelldoc/cmd/shelldoc/cmd"
+import "github.com/mirkoboehm/shelldoc/cmd"
 
 func main() {
 	cmd.Execute()
