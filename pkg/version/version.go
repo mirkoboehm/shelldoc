e117eb80e69c80861da0ca2f1d39bb0bc2b341df
Mirko Boehm <mirko@kde.org>
2026-02-13 11:20:44 +0100

    Fix version output for go install builds
    Closes #49

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -4,9 +4,20 @@
 
 package version
 
-var versionString = "undefined"
+import "runtime/debug"
+
+var versionString = ""
 
 // Version returns the program version as a string.
+// If the version was set via ldflags at build time, that value is returned.
+// Otherwise, it uses Go's built-in build info which includes VCS details
+// when installed via "go install".
 func Version() string {
-	return versionString
+	if versionString != "" {
+		return versionString
+	}
+	if info, ok := debug.ReadBuildInfo(); ok && info.Main.Version != "" {
+		return info.Main.Version
+	}
+	return "unknown"
 }
